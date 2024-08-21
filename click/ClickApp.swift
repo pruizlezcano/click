619f3a6a1f7d52850b4d12f911a3a55bceb48261
Pablo Ruiz <37554617+pruizlezcano@users.noreply.github.com>
2024-08-21 15:54:03 +0200

    Initial commit

---
+++ click/ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickApp.swift
+//  click
+//
+//  Created by Pablo Ruiz on 21/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ click/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  click
+//
+//  Created by Pablo Ruiz on 21/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
